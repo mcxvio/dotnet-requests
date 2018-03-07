f7a505779d446f95380f72bb2431738cf3a1bc62
Marcus <mcxvio@outlook.com>
2018-03-07 12:46:18 +0000

    Serialization & Reading More Information

--- Program.cs
+++ Program.cs
@@ -14,21 +14,31 @@ namespace dotnet_requests
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            ProcessRepositories().Wait();
+            var repositories = ProcessRepositories().Result;
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine();
+            }
         }
-        private static async Task ProcessRepositories()
+
+        private static async Task<List<Repository>> ProcessRepositories()
         {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
             var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
 
-            foreach (var repo in repositories)
-                Console.WriteLine(repo.name);
+            return repositories;
         }
     }
 }
--- repo.cs
+++ repo.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Runtime.Serialization;
 
 namespace WebAPIClient
 {
-    public class repo
+    [DataContract(Name="repo")]
+    public class Repository
     {
-        public string name;
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name="description")]
+        public string Description { get; set; }
+
+        [DataMember(Name="html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name="homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name="watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name="pushed_at")]
+        private string JsonDate { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
     }
 }
